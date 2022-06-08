7bdca335c132572cf9aa7987d61053b26f2438fe
mrok-creator <96848066+mrok-creator@users.noreply.github.com>
2022-06-08 15:47:00 +0000

    Deploying to gh-pages from @ mrok-creator/goit-react-hw-08-phonebook@5284581a01cb23ba4a7d864381f0e3de0de2ad20 🚀

---
+++ static/js/77.3eba8c28.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[77],{5077:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(9358),c=(n(184),function(){r.Z})},9358:function(e,t,n){n.d(t,{Z:function(){return s}});var r="section_section__QWjNu",c="section_title__PalJW",a=n(184);function i(e){var t=e.title,n=e.children;return(0,a.jsxs)("div",{className:r,children:[(0,a.jsx)("h2",{className:c,children:t}),n]})}i.defaultProps={title:""};var s=i}}]);
+//# sourceMappingURL=77.3eba8c28.chunk.js.map
