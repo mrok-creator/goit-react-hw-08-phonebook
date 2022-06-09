3026803021debab9d11c1b5b67e30f03aead1fcf
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-06-09 16:52:12 +0300

    remove unusable const

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import useIsLogin from 'shared/hooks/useIsLogin';
 import Routes from './Route';
 
 import { getCurrentUser } from 'redux/User/user-operation';
 
 export const App = () => {
   const dispatch = useDispatch();
-
-  const isLogin = useIsLogin();
 
   useEffect(() => {
     dispatch(getCurrentUser());
